b11e49a93ba0a72c41c15a8d67a0bf200f8b326b
Parmy <hcl.ihg@gmail.com>
2022-12-21 19:15:15 +0000

    template implementation

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,22 @@
 # Recipe:: default
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
+#
+# Cookbook:: fCreate
+# Recipe:: default
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
+#node['ipaddress']
+#node['memory']['total']
+
+#print statement I have 4 apples
+apple_count = 4
+puts "I have #{apple_count} apples"
+
+template '/root/fCreate.txt' do
+  source 'fCreate.erb'
+  variables( 
+   :ac => "#{apple_count}"
+  )
+end
+#include_recipe 'fUpdate::uRecipe'
